45c87d2b97cb7d49ea4cb2587d645aeb9f4ee834
Donovan Solms <donovan.solms@iliveit.co.za>
2017-07-20 12:03:45 +0200

    Base of the web launcher added

---
+++ run-browsersync.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+browser-sync start --server "web" --files "web/css/*.css,web/js/*.js,web/*.html"
---
+++ web/js/launcher.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $.backstretch([
+      "images/backgrounds/bg01.jpg",
+      "images/backgrounds/bg02.jpg",
+      "images/backgrounds/bg03.jpg",
+      "images/backgrounds/bg04.jpg"
+  ], {duration: 10000, fade: 3000});
+});
