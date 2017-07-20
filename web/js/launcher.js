88ea300a98773653acc1cd7c0860da54c4b5779f
Donovan Solms <donovan.solms@iliveit.co.za>
2017-07-20 21:03:34 +0200

    Added 80% of the web interface

--- web/js/launcher.js
+++ web/js/launcher.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
   $.backstretch([
       "images/backgrounds/bg01.jpg",
-      "images/backgrounds/bg02.jpg",
-      "images/backgrounds/bg03.jpg",
+      //"images/backgrounds/bg02.jpg",
+      //"images/backgrounds/bg03.jpg",
       "images/backgrounds/bg04.jpg"
   ], {duration: 10000, fade: 3000});
+
+  $('#content_pane').niceScroll({
+    cursorborder: "none",
+    cursorcolor: "#F1F1F1",
+    cursorwidth: "10px",
+     railpadding: { top: 4, right: 10, left: 0, bottom: 4 },
+     cursoropacitymin: 0.2,
+     cursoropacitymax: 0.5,
+  });
 });
